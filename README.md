c86122e70ba218ba3645065ff104e8203b200812
Joel Lehtonen <joel.lehtonen@etteplan.com>
2017-09-06 17:53:55 +0300

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# pulse-openconnect-helper
-Browser script for connecting to Juniper Pulse Connect Secure VPN using openconnect
+<!-- -*- mode: markdown; -*- -->
+# Pulse-openconnect-helper
+
+Browser script for connecting to Juniper Pulse Connect Secure VPN
+using openconnect. Works even with the most hostile
+two-factor-authentication schemes.
+
+## How it works?
+
+It's a user script installed to browser which steals the session cookie and shows the user a oneliner with the session cookie and other parameters for openconnect.
+
+![Screenshot](screenshot.png)
+
+## Installation
+
+1. Install [Greasemonkey](https://addons.mozilla.org/fi/firefox/addon/greasemonkey/) for Firefox or [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) for Chrome/Chromium.
+2. Open [pulse-openconnect.user.js](pulse-openconnect.user.js)
+
+## Usage
+
+Remember to use a browser which has the script installed.
+
+1. Login to the VPN provider page using your browser as usual
+2. Press *Copy* on the Openconnect line
+3. Paste to terminal.
+
+## License
+
+[LICENSE](MIT License)
---
+++ pulse-openconnect.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name          Pulse-openconnect
+// @namespace     http://nalpette.io/pulse-openconnect
+// @description   Shows command line instructions how to connect to Juniper Pulse Connect Secure VPN using openconnect
+// @include       https://*/dana/home/index.cgi
+// @grant         GM_setClipboard
+// ==/UserScript==
+
+var cookie = document.cookie.replace(/(?:(?:^|.*;\s*)DSID\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+var connectStr = `sudo openconnect --proto=nc -C DSID=${cookie} ${document.domain}`;
+
+document.getElementById("table_sessions_16").getElementsByTagName("tbody")[0].insertAdjacentHTML('beforeEnd','<tr><td><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAwFBMVEX59Ono1aympqj9/f3t1JF2dnfz8/PnsgK3uLuMjI5lZmnVoA4KCgrV1NaSe0tMS0zmqgDLy8ypegL+xQU2KxP9/fv5wAuxgwXpynQlJCT4+Pg4ODrm5ujMzdCNeToaGx73uwDs7Ozf39/WvYjgrBL8+vScg1FrZVo8PkKtrrRXSjMeEwPRwaSwj0TQ0tzd3eS9kyTg1sf50Ffw8/r6/f9bXWLSnAHp497ZrTf/2ASVi27AwMHdu3L/zQkAAAD///84eWF+AAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQfWAgwVGzHaoBNxAAAAqElEQVQY003M1w6CQBQEUHpZ6UVYukhsiKKxo3v//68ELNl5m5PMMMwYNY8VhopNCMlTCrhVjjSVAmWZPWKRBucue4ieGO0c4fLfS5TsQSfmH0wR+Cgxye9Wxwnv89Bp+AtIhMj3YZaT7gOYA2kATNZjZ6tsB/0krGt26KHlyl4KcLo5l+MIxdRtdZDU4OXKQg/PiVVYm3NMArcSxo/rtjE4W08XzaFvbyG+EP8jeSz5AAAAAElFTkSuQmCC" border="0"></td><td>Openconnect: <tt id="openconnect"></tt><button style="margin-left:1em" id="openconnect_copy">📋 Copy</button></td></tr>');
+document.getElementById("openconnect").insertAdjacentText("beforeEnd", connectStr);
+document.addEventListener("click",function() { GM_setClipboard(connectStr) });
